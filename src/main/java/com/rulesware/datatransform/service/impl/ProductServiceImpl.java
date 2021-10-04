544d02134807d7d9c3553c6725cc5e926d5b7178
nelsonmiranda <39894883+nelsonmiranda@users.noreply.github.com>
2021-10-04 13:48:58 -0600

    Update ProductServiceImpl.java
    Updated to add double commas

--- src/main/java/com/rulesware/datatransform/service/impl/ProductServiceImpl.java
+++ src/main/java/com/rulesware/datatransform/service/impl/ProductServiceImpl.java
@@ -14,9 +14,9 @@ public class ProductServiceImpl implements ProductService{
 	private static List<Product> products = new ArrayList<>();
 	
 	static {
-		Product product1 = new Product(1001, "Laptop Hp", "Intel i7, 4 GB RAM ", 1000, "Laptop");
-		Product product2 = new Product(1002, "Laptop Hp", "Intel i5, 8 GB RAM ", 900, "Laptop");
-		Product product3 = new Product(1003, "Keyboard", "Gaming keyboard",150, "Accessory");
+		Product product1 = new Product(1001, "'Laptop Hp'", "Intel i7, 4 GB RAM ", 1000, "Laptop");
+		Product product2 = new Product(1002, "\"Laptop Hp\"", "Intel i5, 8 GB RAM ", 900, "Laptop");
+		Product product3 = new Product(1003, "'Keyboard'", "Gaming keyboard",150, "Accessory");
 		Product product4 = new Product(1004, "Webcam", "1080p live streaming webcam", 50, "Accessory" );
 		Product product5 = new Product(1005, "iBUYPOWER PC", "ALL IN ONE, AMD RYZEN 5 4500U", 700, "PC");
 		Product product6 = new Product(1006, "CyberpowerPC", "Gamer Xtreme VR Gaming PC", 800, "PC");
